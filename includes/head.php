d33cf24ffdde45994382788458267a58c5146b40
Piotr Białek <p.bialek@healthlabs.pl>
2026-02-27 16:48:51 +0100

    background and popup fix

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Białaszki biorą ślub!!">
     <meta name="keywords" content="wesele, ślub, Białaszki, wedding, invitation">
